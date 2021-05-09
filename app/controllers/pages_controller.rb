ed93f67332a2d2ceeb1b8112a124cdcd9f753e0a
Nico Rithner <nicorithner@gmail.com>
2021-05-09 10:00:57 -0600

    Add @random_capital to index
    It renders a random capital when
    the index page renders

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,10 @@ require 'csv'
 class PagesController < ApplicationController
   def index
     data = CSV.read('lib/assets/data/capitals.csv')
-    @capitals = []
+    capitals = []
     data.each do |arr|
-      arr != data[0] && @capitals << { country: arr[0], capital: arr[1], lat: arr[2].to_f, lon: arr[3].to_f }
+      arr != data[0] && capitals << { country: arr[0], capital: arr[1], lat: arr[2].to_f, lon: arr[3].to_f }
     end
+    @random_capital = capitals.shuffle
   end
 end
