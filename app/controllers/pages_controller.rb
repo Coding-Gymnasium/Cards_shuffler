13d5d843a92445c9b5780b6eb490b049f8d2c0ce
Nico Rithner <nicorithner@gmail.com>
2021-05-09 08:57:35 -0600

    Convert csv row to objects
    Render first object in index page
    Not randomized yet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
+require 'csv'
+
 class PagesController < ApplicationController
-  def index; end
+  def index
+    data = CSV.read('lib/assets/data/capitals.csv')
+    @capitals = []
+    data.each do |arr|
+      arr != data[0] && @capitals << { country: arr[0], capital: arr[1], lat: arr[2].to_f, lon: arr[3].to_f }
+    end
+  end
 end
--- spec/features/pages/index_spec.rb
+++ spec/features/pages/index_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Index Page Features' do
-  describe "As a user" do
-    it 'Displays a card with a country and its capital' do
+  describe 'As a user' do
+    before :each do
       visit root_path
+    end
 
-      expect(page).to have_content"Discover The Capital Cities of The World"
+    it 'Displays a card with a country and its capital' do
+      expect(page).to have_content('Discover The Capital Cities Of The World')
 
       within '.cardsContainer' do
-        within '.card' do 
-          expect(page).to have_css(h2)
-          expect(page).to have_css(h3)
+        within '.card' do
+          expect(page).to have_content('Capital of:')
         end
 
         expect(page).to have_button('Click To Discover')
       end
     end
-
   end
 end
