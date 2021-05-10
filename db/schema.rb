33135f00f672db56f47145a06be00ce1b8254303
Nico Rithner <nicorithner@gmail.com>
2021-05-10 10:08:48 -0600

    Generate Card model
    Created card model spec
    Ran migrations
    All tests passing.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_154710) do
+ActiveRecord::Schema.define(version: 2021_05_10_160138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "name"
+    t.string "country"
+    t.float "lan"
+    t.float "lon"
+    t.bigint "deck_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["deck_id"], name: "index_cards_on_deck_id"
+  end
 
   create_table "decks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cards", "decks"
 end
