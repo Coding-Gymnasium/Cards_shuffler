caaee2eaf84ae3e5dad3b76a859a1fa8b78ed73c
Nico Rithner <nicorithner@gmail.com>
2021-05-10 15:45:21 -0600

    Add test a decks has cards

--- spec/features/pages/index_spec.rb
+++ spec/features/pages/index_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Index page features', type: :feature do
-  before :each do
-    @deck = Deck.create
-  end
-
   it 'Visiting the index page creates a deck of cards' do
     expect { visit root_path }.to change { Deck.count }.by(1)
   end
 
-  xit 'A deck has cards' do
-    binding.pry
-    expect(@deck.cards.count).to_not eq(0)
+  it 'A deck has cards' do
+    visit root_path
+    expect(Deck.last.cards.count).to_not eq(0)
   end
 
   xit 'Deck cards are listed in alphabetical order' do
@@ -19,5 +15,5 @@ RSpec.describe 'Index page features', type: :feature do
 
   xit 'There is shuffle button that when clicked shuffles the cards' do
   end
-
 end
+
