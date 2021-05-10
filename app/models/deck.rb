63f1b0f2f4ee23bdf9b6aa3c62c11b1663e1ab4e
Nico Rithner <nicorithner@gmail.com>
2021-05-10 13:22:44 -0600

    Create generate_deck method and test

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,12 @@
+require 'csv'
+
 class Deck < ApplicationRecord
   has_many :cards
+
+  def generate_deck(id)
+    data = CSV.read('lib/assets/data/capitals.csv')
+    data.each do |row|
+      row != data[0] && Card.create!(name: row[1], country: row[0], lat: row[2].to_f, lon: row[3].to_f, deck_id: id)
+    end
+  end
 end
