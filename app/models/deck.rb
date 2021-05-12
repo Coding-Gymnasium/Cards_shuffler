446c2548325b3d997249ce31a5419f1e3b0b5a49
Nico Rithner <nicorithner@gmail.com>
2021-05-11 19:38:43 -0600

    Add map to card in generate_deck method

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -6,7 +6,17 @@ class Deck < ApplicationRecord
   def generate_deck(id)
     data = CSV.read('lib/assets/data/capitals.csv')
     data.each do |row|
-      row != data[0] && Card.create!(name: row[1], country: row[0], lat: row[2].to_f, lon: row[3].to_f, deck_id: id)
+      map_s = MapService.get_small_map({ lat: row[2], lon: row[3] })
+      map_l = MapService.get_large_map({ lat: row[2], lon: row[3] })
+      row != data[0] && Card.create!(
+        name: row[1],
+        country: row[0],
+        lat: row[2].to_f,
+        lon: row[3].to_f,
+        map_s: map_s,
+        map_l: map_l,
+        deck_id: id
+      )
     end
   end
 end
