ff5fa4ccac3a0895c6f1650410a3fb67fa20cb75
Nico Rithner <nicorithner@gmail.com>
2021-05-11 19:30:22 -0600

    Create map_service_spec and map_service

---
+++ spec/services/map_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe MapService do
+  it 'Makes and API request to Mapquest' do
+    map = MapService.get_small_map({ lat: 39.738453, lon: -104.984853 })
+
+    expect(map).to be_a(String)
+    expect(map).to include('https://www.mapquestapi.com/staticmap/v5/map?')
+  end
+end
