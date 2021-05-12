c2f7d92e52180b979d8b0138a94930b6468fa0dc
Nico Rithner <nicorithner@gmail.com>
2021-05-11 18:59:33 -0600

    Add small and large map to Card table
    Updated model spec, model, and
    card in deck generation.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_160138) do
+ActiveRecord::Schema.define(version: 2021_05_12_000608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_05_10_160138) do
     t.bigint "deck_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "map_s"
+    t.text "map_l"
     t.index ["deck_id"], name: "index_cards_on_deck_id"
   end
 
