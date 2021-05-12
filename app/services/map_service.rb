cca39bd740d28e825fcb48da9a26247ceab3d51d
Nico Rithner <nicorithner@gmail.com>
2021-05-12 12:27:03 -0600

    Update map image size

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -5,7 +5,7 @@ class MapService
   end
 
   def self.get_large_map(coordinates)
-    "https://www.mapquestapi.com/staticmap/v5/map?key=#{ENV["MAP_API_KEY"]}&locations=#{coordinates[:lat]},#{coordinates[:lon]}&defaultMarker=marker-sm-3B5998-22407F&size=400,400@2x&zoom=7"
+    "https://www.mapquestapi.com/staticmap/v5/map?key=#{ENV["MAP_API_KEY"]}&locations=#{coordinates[:lat]},#{coordinates[:lon]}&defaultMarker=marker-sm-3B5998-22407F&size=300,300@2x&zoom=7"
   end
 end
 
