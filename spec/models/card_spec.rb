478845fcf016ffd0632ce5234ce5a7f6197cbc76
Nico Rithner <nicorithner@gmail.com>
2021-05-14 15:26:15 -0600

    Create capital_weather method and spec

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -17,4 +17,35 @@ RSpec.describe Card, type: :model do
   describe 'Relationships' do
     it { should have_many(:decks).through(:deck_cards) }
   end
+
+  describe 'Instance Methods' do
+    before :each do
+      @card = Card.create(
+        name: 'Afghanistan',
+        capital: 'Kabul',
+        population: 27_657_145,
+        lat: 33.0,
+        lon: 65.0,
+        timezones: 'Saturday, 05/15/21 -  01:15:13',
+        currencies: 'Afghan afghani',
+        languages: 'Pashto, Uzbek, Turkmen',
+        flag: 'https://restcountries.eu/data/afg.svg',
+        map: 'https://www.mapquestapi.com/staticmap/v5/map?key=g...',
+        alpha2Code: 'AF'
+      )
+    end
+
+    it '#capital_weather' do
+      VCR.use_cassette('capital_weather') do
+        weather = @card.capital_weather
+
+        expect(weather[:temp]).to be_an(Numeric)
+        expect(weather[:temp_min]).to be_an(Numeric)
+        expect(weather[:temp_max]).to be_an(Numeric)
+        expect(weather[:humidity]).to be_an(Numeric)
+        expect(weather[:description]).to be_a(String)
+        expect(weather[:conditions]).to be_a(String)
+      end
+    end
+  end
 end
