99bad9d935af96b44286b4195674eedb759ed14a
Nico Rithner <nicorithner@gmail.com>
2021-05-14 08:53:58 -0600

    Update views

--- app/javascript/packs/featuredCard.js
+++ app/javascript/packs/featuredCard.js
@@ -3,7 +3,7 @@ export default function onLoad() {
   var cards = document.getElementById('cards').getElementsByTagName('div')
   featuredCard.replaceChildren(cards[0].cloneNode(true))
   var mapImage = document.getElementById('featuredCard').getElementsByTagName('img')[0]
-  mapImage.style.height = '600px'
-  mapImage.style.width = '600px'
+  mapImage.style.height = '30%'
+  mapImage.style.width = '30%'
 }
 window.addEventListener('load', onLoad)
