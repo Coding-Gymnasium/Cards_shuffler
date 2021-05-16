ff2a38edcbf91e4859254abe68ecec50018f0b44
Nico Rithner <nicorithner@gmail.com>
2021-05-16 10:35:39 -0600

    Update map query

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,6 +1,19 @@
 class MapService
+
   def self.get_map(coordinates)
-    "https://www.mapquestapi.com/staticmap/v5/map?key=#{ENV["MAP_API_KEY"]}&locations=#{coordinates[:lat]},#{coordinates[:lon]}&defaultMarker=marker-sm-3B5998-22407F&size=200,200@2x&zoom=7"
+    #response = conn.get('staticmap/v5/map?') do |f|
+    #  f.params[:key] = ENV["MAP_API_KEY"]
+    #  f.params[:center] = "#{coordinates[:lat]},#{coordinates[:lon]}"
+    #  f.params[:zoom] = 3
+    #end
+
+    #JSON.parse(response.body, symbolize_names: true)
+
+    "https://www.mapquestapi.com/staticmap/v5/map?key=#{ENV["MAP_API_KEY"]}&center=#{coordinates[:lat]},#{coordinates[:lon]}&size=400,200@2x&zoom=3"
   end
+
+  #def self.conn
+  #  Faraday.new('https://www.mapquestapi.com')
+  #end
 end
 
