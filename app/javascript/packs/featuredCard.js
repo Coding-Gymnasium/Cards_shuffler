d890e079111016b9e40dc3791f922d2f6b45d41e
Nico Rithner <nicorithner@gmail.com>
2021-05-16 16:56:38 -0600

    Remove image sizing

--- app/javascript/packs/featuredCard.js
+++ app/javascript/packs/featuredCard.js
@@ -3,7 +3,5 @@ export default function onLoad() {
   var cards = document.getElementById('cards').getElementsByTagName('div')
   featuredCard.replaceChildren(cards[0].cloneNode(true))
   var mapImage = document.getElementById('featuredCard').getElementsByTagName('img')[0]
-  mapImage.style.height = '30%'
-  mapImage.style.width = '30%'
 }
 window.addEventListener('load', onLoad)
