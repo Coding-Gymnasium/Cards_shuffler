df566e8b946697276c6f8ac1b3eba12ce8812eb0
Nico Rithner <nicorithner@gmail.com>
2021-05-24 15:44:20 -0600

    Update vcr fixtures. Fixed alignment shuffle.js

--- app/javascript/packs/shuffle.js
+++ app/javascript/packs/shuffle.js
@@ -2,15 +2,15 @@ import onLoad from './featuredCard.js'
 
 document.addEventListener("turbolinks:load", ()=> {
 
-const cardContainer = document.getElementById("cards")
-const button = document.getElementById("shuffle")
+  const cardContainer = document.getElementById("cards")
+  const button = document.getElementById("shuffle")
 
-button.addEventListener("click", (event) => {
-  let cards = document.getElementsByClassName("card")
+  button.addEventListener("click", (event) => {
+    let cards = document.getElementsByClassName("card")
 
-  let shuffled = _.shuffle(cards)
-  cardContainer.innerHTML = ""
-  shuffled.forEach(element => cardContainer.appendChild(element))
-  onLoad()
-})
+    let shuffled = _.shuffle(cards)
+    cardContainer.innerHTML = ""
+    shuffled.forEach(element => cardContainer.appendChild(element))
+    onLoad()
+  })
 });
