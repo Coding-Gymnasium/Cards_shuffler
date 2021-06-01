a1081d7eb7624bd8f1fbc3291f1e7fe9c2d69f85
Nico Rithner <nicorithner@gmail.com>
2021-05-31 18:57:53 -0600

    Migrate current_time method from deck to card

--- app/models/card.rb
+++ app/models/card.rb
@@ -31,9 +31,23 @@ class Card < ApplicationRecord
         count = 0
         self.images.attach(io: File.open(tempfile), filename: "#{self.name}-#{count += 1}")
       end
-    else
-      self.images
+     self.images
     end
+  end
+
+  def current_time(off_set)
+    arr = plus_or_minus(off_set)
+    n = extract_numbers(arr)
+    sec = n.map { |num| (num == n[0] && num * 60 * 60) || (num == n[1] && num * 60) }.sum
+    (Time.now.utc + sec).strftime('%A, %D -  %T')
+  end
+
+  def plus_or_minus(string)
+    string.include?('+') ? string.split(/[+:]/) : string.split(/[-:]/)
+  end
+
+  def extract_numbers(off_set_array)
+    off_set_array.map { |e| e.to_i if e != off_set_array[0] }.compact
   end
 end
 
